e1a1578a10918e5e5833e2cd11da413156211f02
craziemac <marcus.jonsson.mj+git@gmail.com>
2015-01-27 23:03:30 +0100

    crypto

--- core/authentication.js
+++ core/authentication.js
@@ -35,7 +35,7 @@ exports.basicAuth = function(conf) {
         }
         var cipher = crypto.createCipher('aes-256-cbc', conf.application.salt);
         cipher.update(user.pass, 'utf8', 'base64');
-    console.log('password %s username %s', user.pass, user.name);
+console.log('password %s username %s', cipher.final('base64'), user.name);
 
         if (!user || !(cipher.final('base64') === conf.application.password) && user.name === conf.application.username ) {
             res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
